1a946fed58dda6e5621369fb2728d5ac8ba671e4
Vasudev <shajcheruvath@gmail.com>
2026-07-10 21:14:52 +0530

    Infinity WAR

--- Django_test/Django_test/settings.py
+++ Django_test/Django_test/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_5a25%n3-1^2&76+mks(u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "AlongTheSkies.pythonanywhere.com"]
 
 
 # Application definition
@@ -105,6 +105,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://cloudfile-wnob.onrender.com",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+    "https://alongtheskies.pythonanywhere.com",
 ]
 
 
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
