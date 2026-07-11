259897fc4cda73f7f8246ee7be4ca72d4622f97b
Vasudev <shajcheruvath@gmail.com>
2026-07-11 12:34:08 +0530

    H

--- Django_test/CloudFile/views.py
+++ Django_test/CloudFile/views.py
@@ -24,7 +24,8 @@ def get_client_ip(request):
 
 #================= Views ======================
 def home(request):
-    return render(request, 'CloudFile/home.html')
+    files = SharedFile.objects.filter(owner=request.user).order_by('-uploaded_at')
+    return render(request, 'CloudFile/home.html', {'files': files})
 
 @login_required
 def upload_file(request):
